494f125c11f9c08ec4e4e6fec484a026b9824767
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-04-20 08:25:57 -0400

    Added listener to :taco:.

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const app = new App({
 
 // Says hello when app home is opened
 app.event('app_home_opened', ({ event, say }) => {  
-    say(`Hello world, <@${event.user}>!`);
+    say(`Hello <@${event.user}>!`);
 });
+
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
     console.log(message.text);
+    console.log(message.user);
+
     // say() sends a message to the channel where the event was triggered
     await say({
         blocks: [
@@ -37,10 +40,35 @@ app.message('hello', async ({ message, say }) => {
       });
     });
 
+  // Listens to incoming messages that contain ":bread:"
   app.message(':bread:', async ({ message, say }) => {
     console.log(message.text);
-    await say(`<@${message.user}> wants to give bread to someone!`);
+    let giver = message.user;
+    let receivers = [];
+    // Parse for <@{username}> and check if currentUser is in the message as well
+    // TODO figure out if username is actually a user 
+
+    if (receivers === []){
+      await say(`<@${message.user}> wants to give bread to someone!`);
+    }
   }); 
+
+
+// Listens to incoming messages that contain ":taco:"
+app.message(':taco:', async ({ message, say }) => {
+  let newMessage = `This is not the heytaco app...
+  It's raining tacos
+  From out of the sky
+  Tacos
+  No need to ask why
+  Just open your mouth and close your eyes
+  It's raining tacos.
+
+  You can sing the rest of the song now :P!
+  `
+  await say(newMessage);
+}); 
+
 
     
 // It looks like the action method has been depreciated and shortcut method is supposed to work here.    
@@ -49,6 +77,14 @@ app.shortcut('button_click', async ({ body, ack, say }) => {
     await ack();
     await say(`<@${body.user.id}> clicked the button`);
     });   
+
+
+// Determine if username is a actually user
+isUser = (username) => {
+  // TODO use RESTAPI call https://kwackjrpraylude.slack.com/api/users.list
+
+}
+
 
 (async () => {
   // Start your app
