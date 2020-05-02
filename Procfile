0c62c4fac238025e2806733a28282d13fb2e41b6
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-05-02 09:34:27 -0400

    A fix for the scheduler.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: node app.js
-clock: python clock.py
+clock: python3 clock.py
--- clock.py
+++ clock.py
@@ -1,13 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 
-sched = BlockingScheduler()
-
-
-@sched.scheduled_job('cron', hour=23)
 def scheduled_job():
+    print('This job runs everyday at 11pm.')
     req = requests.post('https://kwack-app.herokuapp.com/replenish')
     print(req.status_code)
-    print('This job is run everyday at 11pm.')
+    
 
-sched.start()
+if __name__ == "__main__":
+    sched = BlockingScheduler()
+    sched.add_job(scheduled_job, 'cron', id='scheduled_job', hour=23)
+
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
